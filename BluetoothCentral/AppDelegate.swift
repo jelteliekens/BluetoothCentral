9bbf41d63b413902d0c874ad2ddbc9e74ab4dced
Jelte Liekens <jelte@wieni.be>
2016-08-06 18:22:39 +0200

    Initial Commit

---
+++ BluetoothCentral/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BluetoothCentral
+//
+//  Created by Jelte Liekens on 06/08/16.
+//  Copyright © 2016 Jelte Liekens. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BluetoothCentral/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BluetoothCentral
+//
+//  Created by Jelte Liekens on 06/08/16.
+//  Copyright © 2016 Jelte Liekens. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
